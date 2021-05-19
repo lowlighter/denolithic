13192135af06edf754fec063221fc681b34ed0cf
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-05-20 00:39:22 +0200

    Update eval.ts

--- api/eval.ts
+++ api/eval.ts
@@ -3,7 +3,7 @@ import type { ServerRequest } from "https://deno.land/std@0.97.0/http/server.ts"
 import { readAll } from "https://deno.land/std@0.97.0/io/util.ts"
 
 //Maximum runtime execution
-const TIMEOUT = 5*1000
+const TIMEOUT = 3*1000
 
 /** Request handler */
 export default async function (request: ServerRequest) {
@@ -14,16 +14,19 @@ export default async function (request: ServerRequest) {
   const params = new URLSearchParams(request.url.split("?")[1] ?? "")
   const script = new TextDecoder().decode(await readAll(request.body))
   //Spawn process
+  let killed = false
+  const time = performance.now()
   const process = Deno.run({cmd:["deno", "eval", "--ext", params.get("ext") ?? "ts", "--seed", "0", "--unstable", "--no-check", script], stdout: "piped", stderr: "piped"})
   try {
     //Wait for process (kill if needed)
-    const timeout = setTimeout(() => process.kill(Deno.Signal.SIGKILL), TIMEOUT)
+    const timeout = setTimeout(() => { killed = true ; process.kill(Deno.Signal.SIGKILL) }, TIMEOUT)
     const [{success, code}, stdout, stderr] = await Promise.all([process.status(), process.output(), process.stderrOutput()])
     clearTimeout(timeout)
     //Send back status and output
     const headers = new Headers()
-    headers.set("x-success", `${Number(success)}`)
+    headers.set("x-success", `${killed ? -1 : Number(success)}`)
     headers.set("x-code", `${code}`)
+    headers.set("x-duration", `${performance.now()-time}`)
     return request.respond({status:200, headers, body:new Uint8Array([...stdout, ...stderr])})
   }
   catch {

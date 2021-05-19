ae79e9deed500c8d9d4d3ca985cfb0d40bfd3ad4
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-05-19 12:33:39 +0200

    Initial commit

---
+++ api/eval.ts
@@ -0,0 +1,9 @@
+import { ServerRequest } from "https://deno.land/std@0.97.0/http/server.ts";
+import { readAll } from "https://deno.land/std@0.97.0/io/util.ts";
+
+export default async (req: ServerRequest) =>
+  req.method === "POST"
+    ? req.respond({
+      body: eval(new TextDecoder().decode(await readAll(req.body))),
+    })
+    : req.respond({ status: 405 });

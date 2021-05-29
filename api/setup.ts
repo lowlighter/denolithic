c4a8615fecdee473f19aeb8b057165d8d4571670
louis <louis@explorer-ant.home.arpa>
2021-05-29 22:07:58 +0200

    UPDATE Add setElement()

--- api/setup.ts
+++ api/setup.ts
@@ -2,8 +2,16 @@
 import { parse,  stringify } from "https://deno.land/std@0.97.0/encoding/yaml.ts";
 import { DOMParser, Element } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
+//Create a new DOM element with the given attributes
+function setElement(type: string, attrs: any){
+    let elem = html.createElement(type)
+    for (const [key, value] of Object.entries(attrs))
+        elem.setAttribute(key, value)
+    return elem
+}
+
 //Load static/base.html
-let html: any = new DOMParser().parseFromString(await Deno.readTextFile("./template/index.html"), "text/html")
+let html: any = new DOMParser().parseFromString(await Deno.readTextFile("./templates/index.html"), "text/html")
 
 //Load config.yml
 let config: any = parse(await Deno.readTextFile("./config.yml"))
@@ -19,10 +27,7 @@ config["styles"]?.forEach((stylePath: string ) => {
         "href" : `static/${stylePath}`,
         "rel": "stylesheet"
     }
-    let elem = html.createElement("link")
-    for (const [key, value] of Object.entries(attrs))
-        elem.setAttribute(key, value)
-    html.head.appendChild(elem)
+    html.head.appendChild(setElement("link", attrs))
 })
 
 //Set plugins dependecies
@@ -31,28 +36,21 @@ config["plugins"].forEach((element: string ) => {
     switch(element){
         case "katex" : {
             // Add css
-            let attrs = {
+            html.body.appendChild(setElement("link", {
                 "href" : "https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css",
                 "integrity": "sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc",
                 "crossorigin": "anonymous",
                 "rel": "stylesheet"
-            }
-            let elem = html.createElement("link")
-            for (const [key, value] of Object.entries(attrs))
-                elem.setAttribute(key, value)
-            html.body.appendChild(elem)
+            }))
 
             // Add min.js
-            let attrs2 = {
+            let elemScript = setElement("link", {
                 "href" : "https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.js",
                 "integrity": "sha384-YNHdsYkH6gMx9y3mRkmcJ2mFUjTd0qNQQvY9VYZgQd7DcN7env35GzlmFaZ23JGp",
                 "crossorigin": "anonymous"
-            }
-            elem = html.createElement("script")
-            for (const [key, value] of Object.entries(attrs2))
-                elem.setAttribute(key, value)
-            elem.defer = true
-            html.body.appendChild(elem)
+            })
+            elemScript.defer = true
+            html.body.appendChild(elemScript)
 
             break
         }
@@ -71,3 +69,6 @@ html = `
   </body>
  </html>`;
 Deno.writeTextFileSync("./index.html", html);
+
+// TODO Remove template folder
+// await Deno.remove('hello', { recursive: true });
